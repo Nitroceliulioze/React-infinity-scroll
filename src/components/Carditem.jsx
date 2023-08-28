ac7ac22d3df63cdb1671aeb9a6707aeeed106f12
Nitroceliulioze <aksionova.diana@gmail.com>
2023-08-28 23:00:43 +0300

    overlay on hover

--- src/components/Carditem.jsx
+++ src/components/Carditem.jsx
@@ -1,22 +1,38 @@
+import { useState } from "react";
 import "./cardItem.css";
 
 const Carditem = ({ id, alt, photographer, src, liked }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
   return (
-    <div>
+    <div
+      className="card-container"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
       <div className="card">
-
-          <img src={src.medium} alt={alt}/>
-
+        <img src={src.medium} alt={alt} />
       </div>
-      <div className="overlay">
-        <div className="overlay-content">
-          <span className="title"> {alt} </span>
-          <hr />
-          <span className="author"> {photographer} </span>
-          <br />
-          <button className="fav-btn" type="button">Favourite</button>
+      {isHovered && (
+        <div className="overlay">
+          <div className="overlay-content">
+            <span className="title"> {alt} </span>
+            <hr />
+            <span className="author"> {photographer} </span>
+            <br />
+            <button className="fav-btn" type="button">
+              Favourite
+            </button>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
